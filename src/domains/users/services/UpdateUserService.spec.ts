8b963493c91c962420bbc6d7f7c195be9700a5e7
Lucas Medeiros <freitasmlucas@gmail.com>
2021-03-21 10:45:27 -0300

    update service test coverage

--- src/domains/users/services/UpdateUserService.spec.ts
+++ src/domains/users/services/UpdateUserService.spec.ts
@@ -1,6 +1,29 @@
+import FakeUsersRepository from '@domains/users/repositories/fakes/FakeUsersRepository';
+import CreateUserService from '@domains/users/services/CreateUserService';
+import UpdateUserService from './UpdateUserService';
+
+let fakeUsersRepository: FakeUsersRepository = new FakeUsersRepository();
+let createUserService: CreateUserService;
+let updateUserService: UpdateUserService;
+
 describe('UpdateUser', () => {
-  it('should be update a user');
-  it(
-    'should return error if authenticate user is trying to update another user and his type is not root or admin',
-  );
+  beforeEach(() => {
+    createUserService = new CreateUserService(fakeUsersRepository);
+    updateUserService = new UpdateUserService(fakeUsersRepository);
+  });
+  it('should be update a user', async () => {
+    const user = await createUserService.handle({
+      email: 'testUser@email.com',
+      password: 'test321',
+      name: 'Test User',
+      status: true,
+      type_id: 2,
+    });
+
+    user.name = 'Updated name';
+
+    const updatedUser = await updateUserService.handle(user);
+
+    expect(updatedUser.name).toEqual(user.name);
+  });
 });
