af6aac40763160d24a3b15b8db236b0965dabd04
Lucas Medeiros <freitasmlucas@gmail.com>
2021-03-21 11:41:48 -0300

    created authentication test cases

--- src/domains/users/services/AuthenticateUserService.spec.ts
+++ src/domains/users/services/AuthenticateUserService.spec.ts
@@ -11,7 +11,44 @@ describe('AuthenticateUser', () => {
     createUserService = new CreateUserService(fakeUsersRepository);
     authenticateUserService = new AuthenticateUserService(fakeUsersRepository);
   });
-  it('should be able to authenticate', async () => {});
-  it('should not be able with non existing user', async () => {});
-  it('should not be able to authenticate with wrong password', async () => {});
+  it('should be able to authenticate', async () => {
+    const user = await createUserService.handle({
+      email: 'testUser@email.com',
+      password: 'test321',
+      name: 'Test User',
+      status: true,
+      type_id: 2,
+    });
+
+    const response = await authenticateUserService.handle({
+      email: 'testUser@email.com',
+      password: 'test321',
+    });
+
+    expect(response).toHaveProperty('token');
+  });
+  it('should not be able with non existing user', async () => {
+    expect(
+      authenticateUserService.handle({
+        email: 'nonexisting@email.com',
+        password: 'test321',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+  it('should not be able to authenticate with wrong password', async () => {
+    await createUserService.handle({
+      email: 'testUser2@email.com',
+      password: 'test321',
+      name: 'Test User',
+      status: true,
+      type_id: 2,
+    });
+
+    await expect(
+      authenticateUserService.handle({
+        email: 'testUser2@email.com',
+        password: 'wrong',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
